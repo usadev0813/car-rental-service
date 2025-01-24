2ee57aae7e672fcaa2db016d8cfc7ed923cb8729
jeongho <tjsrsdqq@tscientific.co.kr>
2025-01-24 11:45:40 +0900

    :sparkles: feat : CategoryService 메소드 주석 추가

--- src/main/java/com/carrental/domain/CategoryService.java
+++ src/main/java/com/carrental/domain/CategoryService.java
@@ -15,13 +15,24 @@ public class CategoryService {
 
 	private final CategoryRepository categoryRepository;
 
+	/**
+	 * 주어진 ID 목록에 해당하는 카테고리를 조회합니다.
+	 *
+	 * @param categoryIds 조회할 카테고리 ID 목록
+	 * @return 조회된 카테고리 목록
+	 * @throws IllegalArgumentException 일부 ID에 해당하는 카테고리가 없을 경우 예외 발생
+	 */
 	public List<Category> getCategoriesByIds(List<Long> categoryIds) {
 		List<Category> categories = categoryRepository.findAllById(categoryIds);
 
+		// 조회된 카테고리의 수가 요청된 ID의 수와 다를 경우 예외 처리
 		if (categories.size() != categoryIds.size()) {
+			// 존재하지 않는 카테고리 ID 목록 추출
 			List<Long> missingIds = categoryIds.stream()
 				.filter(id -> categories.stream().noneMatch(category -> category.getId().equals(id)))
 				.toList();
+
+			// 예외 발생
 			throw new IllegalArgumentException("존재하지 않는 카테고리가 포함되어 있습니다: " + missingIds);
 		}
 
