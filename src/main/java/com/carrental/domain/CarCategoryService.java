495881beaba29a6bf2eeb93a5cfc697be3951328
jeongho <tjsrsdqq@tscientific.co.kr>
2025-01-24 11:48:48 +0900

    :sparkles: feat : CarCategoryService 메소드 주석 추가

--- src/main/java/com/carrental/domain/CarCategoryService.java
+++ src/main/java/com/carrental/domain/CarCategoryService.java
@@ -20,17 +20,22 @@ public class CarCategoryService {
 
 	private final CarCategoryRepository carCategoryRepository;
 
+	/**
+	 * 특정 카테고리에 속한 자동차 목록을 조회합니다.
+	 *
+	 * @param categoryId 조회할 카테고리의 ID
+	 * @return 자동차와 해당 자동차가 속한 카테고리 이름 목록을 포함하는 객체 리스트
+	 */
 	public List<CarCategoryDetails> getCarsByCategoryId(Long categoryId) {
-		// 카테고리 ID에 따른 CarCategory 목록 조회
 		List<CarCategory> carCategories = carCategoryRepository.findByCategoryId(categoryId);
 
-		// 자동차별로 카테고리 정보 포함하여 반환
+		// 자동차별로 그룹화하여 각 자동차의 카테고리 이름 목록 생성
 		return carCategories.stream()
-			.collect(Collectors.groupingBy(CarCategory::getCar))
+			.collect(Collectors.groupingBy(CarCategory::getCar)) // 자동차별로 그룹화
 			.entrySet()
 			.stream()
 			.map(entry -> {
-				var car = entry.getKey();
+				var car = entry.getKey(); // 그룹화된 자동차 객체
 				var categories = entry.getValue().stream()
 					.map(carCategory -> carCategory.getCategory().getName())
 					.toList();
@@ -40,12 +45,16 @@ public class CarCategoryService {
 			.toList();
 	}
 
+	/**
+	 * 특정 자동차의 카테고리를 업데이트합니다.
+	 *
+	 * @param car       업데이트할 자동차 객체
+	 * @param categories 새로운 카테고리 리스트
+	 */
 	@Transactional
 	public void updateCarCategories(Car car, List<Category> categories) {
-		// 기존 카테고리 제거
 		carCategoryRepository.deleteByCar(car);
 
-		// 새로운 카테고리 추가
 		List<CarCategory> newCarCategories = categories.stream()
 			.map(category -> new CarCategory(car, category))
 			.toList();
@@ -53,4 +62,3 @@ public class CarCategoryService {
 		carCategoryRepository.saveAll(newCarCategories);
 	}
 }
-
